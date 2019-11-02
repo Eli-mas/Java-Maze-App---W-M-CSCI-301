999d903d0760314c6b341a5872246fc68b7d9f62
ElijahMasWM <eamas@email.wm.edu>
2019-11-02 18:10:39 -0400

    P5 -- commit 5.2a: MazeColor
    (Forgot to include this with commit 5.2)

--- MazeByElijahMas/src/gui/MazeColor.java
+++ MazeByElijahMas/src/gui/MazeColor.java
@@ -1,9 +1,13 @@
 package gui;
 
 import java.awt.Color;
+import java.lang.reflect.Field;
+import java.lang.Class;
 
 public class MazeColor {
 	//internal color representation
+	
+	public static enum Colors {white, red, orange, yellow, grey, gray, black, green, blue, violet, purple, cyan, brown};
 	
 	int r,g,b;
 	
@@ -20,5 +24,14 @@ public class MazeColor {
 	public static Color export(int r, int g, int b){
 		//convert to AWT color
 		return new Color(r, g, b);
+	}
+	
+	public static Color export(MazeColor.Colors c) {
+		try {
+			return (Color)Color.class.getDeclaredField(c.name()).get(null);
+		} catch (Exception e) {
+			// e.printStackTrace()
+			return Color.black;
+		}
 	}
 }
